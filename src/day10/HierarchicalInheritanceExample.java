49059c0e72a4cb90e7510a8fbd9cbfa0e2c181c0
utkrshydv <utkarsh4socials@gmail.com>
2025-01-31 19:07:51 +0530

    Program 6 of Day 10

---
+++ src/day10/HierarchicalInheritanceExample.java
@@ -0,0 +1,34 @@
+package day10;
+
+class Shape{
+    void display() {
+        System.out.println("This is a shape");
+    }
+}
+
+class Circle extends Shape{
+    void area() {
+        System.out.println("Area of circle is πr²");
+    }
+}
+
+class Rectangle extends Shape{
+    void area() {
+        System.out.println("Area of rectangle is length x breadth");
+    }
+}
+
+public class HierarchicalInheritanceExample {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.display();
+        circle.area();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.display();
+        rectangle.area();
+
+    }
+
+}
+
